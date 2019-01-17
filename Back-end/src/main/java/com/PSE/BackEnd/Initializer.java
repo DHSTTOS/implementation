c09f1f1de24db914579bd8776a4cc391cc5f7fe9
Mario <mario.ordiano@gmail.com>
2019-01-17 16:41:35 +0100

    testing, my patience

--- Back-end/src/main/java/com/PSE/BackEnd/Initializer.java
+++ Back-end/src/main/java/com/PSE/BackEnd/Initializer.java
@@ -2,5 +2,9 @@ package com.PSE.BackEnd;
 
 public class Initializer
 {
-
+    public static void main(String[] args)
+    {
+        String string = "ASAAAAAAAAAAAAAAAAa";
+        System.out.println("wgaaaaat?");
+    }
 }
