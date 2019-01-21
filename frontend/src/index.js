99f3680f38908c198b8d9b46630626cb1d338da2
Xiaoru Li <dev@xiaoru.li>
2019-01-21 13:25:49 +0100

    Inject the socket object into context of whole app

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { socket } from "@libs/wsutils";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// Considering to use context instead of directly importing socket in the app...
+const WSContext = React.createContext("ws");
+
+const AppWithContext = () => (
+  <WSContext.Provider value={socket}>
+    <App />
+  </WSContext.Provider>
+);
+
+ReactDOM.render(<AppWithContext />, document.getElementById("root"));
