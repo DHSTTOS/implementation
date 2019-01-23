cf0bdb1a21b277659b8d6dac9eb54dd8ceae6b81
Mario <mario.ordiano@gmail.com>
2019-01-23 01:49:16 +0100

    fixed stupid mistakes

--- Back-end/src/main/java/invalid/adininspector/backend/MongoClientMediator.java
+++ Back-end/src/main/java/invalid/adininspector/backend/MongoClientMediator.java
@@ -36,7 +36,7 @@ public class MongoClientMediator {
         try {
             db.getCollection(collection).insertOne(record.getAsDocument());
         } catch (MongoWriteException ex) {
-            //TODO: how to handle this
+            // TODO: how to handle this
             System.out.println("an entry with this offset already exists %n at offset: " + record.get_id());
         }
     }
@@ -48,66 +48,62 @@ public class MongoClientMediator {
     }
 
     public void ProcessCollection(String collection) {
-        //DataProcessor
+        // DataProcessor
     }
 
-    //TODO: potentially we should convert this to String[] for the hub
-    //do we need this ? potentially not
-    public MongoCollection<Document> getCollection(String collection) {
-        return db.getCollection(collection);
+    // TODO: potentially we should convert this to String[] for the hub
+    // do we need this ? potentially not
+    private String[] getCollection(String collection) {
+        return mongoIteratorToStringArray(db.getCollection(collection).find());
     }
 
-    //find returns a cursor to the first object so we simple return that one
+    // find returns a cursor to the first object so we simple return that one
     public String getStartRecord(String collection) {
         return db.getCollection(collection).find().first().toString();
     }
 
-    //we sort descending by id and then get the first (last object)
+    // we sort descending by id and then get the first (last object)
     public String getEndRecord(String collection) {
         return db.getCollection(collection).find().sort(new Document("_id", -1)).first().toString();
     }
 
-    //an int *should* suffice for now at least
+    // an int *should* suffice for now at least
     public int CollectionSize(String collection) {
-        //count is deprecated, there's an estimation which should work fine but 
-        return (int)db.getCollection(collection).countDocuments();
+        // count is deprecated, there's an estimation which should work fine but
+        return (int) db.getCollection(collection).countDocuments();
     }
 
-    public String[] getRecordInRange(String collection, String key, String start,String end) {
+    public String[] getRecordInRange(String collection, String key, String start, String end) {
         BasicDBObject query = new BasicDBObject();
-        query.put(key , new BasicDBObject("$gt",  Integer.parseInt(start) )
-            .append("$lt", Integer.parseInt(end)));
-        
+        query.put(key, new BasicDBObject("$gt", Integer.parseInt(start)).append("$lt", Integer.parseInt(end)));
+
         return mongoIteratorToStringArray(db.getCollection(collection).find(query));
     }
 
-    public long getRecordsInRangeSize(String collection, String key, String start,String end) {
+    public long getRecordsInRangeSize(String collection, String key, String start, String end) {
         BasicDBObject query = new BasicDBObject();
-        query.put(key , new BasicDBObject("$gt",  Integer.parseInt(start) )
-            .append("$lt", Integer.parseInt(end)));
-        
-        return (int)db.getCollection(collection).countDocuments(query);
+        query.put(key, new BasicDBObject("$gt", Integer.parseInt(start)).append("$lt", Integer.parseInt(end)));
+
+        return (int) db.getCollection(collection).countDocuments(query);
     }
 
-    //get all names of all collections and put em in an Array.
+    // get all names of all collections and put em in an Array.
     public String[] getAvailableCollections() {
         List<String> colls = new ArrayList<>();
 
-         db.listCollectionNames().forEach((Consumer<String>) colls::add);
+        db.listCollectionNames().forEach((Consumer<String>) colls::add);
 
-         //return colls.toArray(new String[colls.size()]);
-         return mongoIteratorToStringArray(db.listCollectionNames());
+        // return colls.toArray(new String[colls.size()]);
+        return mongoIteratorToStringArray(db.listCollectionNames());
     }
 
-
-    private String[] mongoIteratorToStringArray(MongoIterable iterable)
-    {
+    private String[] mongoIteratorToStringArray(MongoIterable iterable) {
         List<String> colls = new ArrayList<>();
 
-        //TODO: fix parametrization
+        // TODO: fix parametrization
         iterable.forEach((Consumer<String>) colls::add);
 
         return colls.toArray(new String[colls.size()]);
-        
+
     }
 }
