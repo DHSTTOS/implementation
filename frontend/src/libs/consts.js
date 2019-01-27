4ac96cdd217d1eea491a263b23dd248433348127
Xiaoru Li <dev@xiaoru.li>
2019-01-27 21:08:25 +0100

    Refactor global filters into real ones

--- frontend/src/libs/consts.js
+++ frontend/src/libs/consts.js
@@ -1,20 +1,13 @@
 export const DEFAULT_SOURCE_NAME = "__default_empty__";
 
 export const DEFAULT_GLOBAL_FILTERS = {
-  layers: {
-    layer1: false,
-    layer2: false,
-    layer3: false,
-    layer4: false,
-    layer5: false,
-    layer6: false,
-    layer7: false,
-  },
-  protocols: {
-    tcp: false,
-    udp: false,
-    // more protocols
-  },
+  ether: false,
+  profinet: false,
+  l2other: false,
+  l3: false,
+  tcp: false,
+  udp: false,
+  l4other: false,
 };
 
 export const SCATTER_PLOT = "Scatter Plot";
