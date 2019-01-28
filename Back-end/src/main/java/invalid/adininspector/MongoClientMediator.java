ea5fd621379ba7f1fb70f3f25ad4c18de5a2af58
Mario <mario.ordiano@gmail.com>
2019-01-29 00:19:51 +0100

    added convinience methods for aggregation

--- Back-end/src/main/java/invalid/adininspector/MongoClientMediator.java
+++ Back-end/src/main/java/invalid/adininspector/MongoClientMediator.java
@@ -29,49 +29,69 @@ public class MongoClientMediator {
     private MongoClient client;
     private MongoDatabase db;
 
-    public MongoClientMediator(String udid, String password, String dbName) throws LoginFailureException
-    {
+    public MongoClientMediator(String udid, String password, String dbName) throws LoginFailureException {
         ServerAddress serverAddr = new ServerAddress("localhost");
 
-        //ScramSha1 is the default auth method from Mongo
-        //TODO this will try and log into the the admin database, this should change
-        //we always use the admin database to check users.
+        // ScramSha1 is the default auth method from Mongo
+        // TODO this will try and log into the the admin database, this should change
+        // we always use the admin database to check users.
         MongoCredential cred = MongoCredential.createScramSha1Credential(udid, "admin", password.toCharArray());
 
-        try{
-        client = new MongoClient(serverAddr, Collections.singletonList(cred));
+        try {
+            client = new MongoClient(serverAddr, Collections.singletonList(cred));
 
-        // get access to the database. I still don't know if doing it like this is
-        // ideal.
-        db = client.getDatabase(dbName);
+            // get access to the database. I still don't know if doing it like this is
+            // ideal.
+            db = client.getDatabase(dbName);
 
-        
-        BasicDBObject ping = new BasicDBObject("ping", "1");
-        db.runCommand(ping);
-        }
-        catch(MongoSecurityException e)
-        {            
+            BasicDBObject ping = new BasicDBObject("ping", "1");
+            db.runCommand(ping);
+        } catch (MongoSecurityException e) {
 
-            //force the caller to handle the exception
+            // force the caller to handle the exception
             throw new LoginFailureException(e.getMessage());
         }
     }
 
-    public MongoClientMediator(String udid, String password)throws LoginFailureException {
+    public MongoClientMediator(String udid, String password) throws LoginFailureException {
         this(udid, password, "test");
     }
 
     public void addRecordToCollection(Record record, String collection) {
         try {
-            //p(record.getAsDocument());
+            // p(record.getAsDocument());
             db.getCollection(collection).insertOne(record.getAsDocument());
             p("inserting to: " + collection + " at offset: " + record.get_id());
         } catch (MongoWriteException ex) {
             // TODO: how to handle this, skip?, overwrite? or compare and decide which one
             // to keep?
-           System.out.println("an entry with this offset already exists at offset: " + record.get_id());
+            //System.out.println("an entry with this offset already exists at offset: " + record.get_id());
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    // we do not necessarly need a record for aggregated records.
+    public void addRecordToCollection(Document record, String collection) {
+        try {
+            // p(record.getAsDocument());
+            db.getCollection(collection).insertOne(record);
+            p("inserting to: " + collection);
+        } catch (MongoWriteException ex) {
+            // TODO: how to handle this, skip?, overwrite? or compare and decide which one
+            // to keep?
+            //System.out.println("an entry with this offset already exists ");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    // we do not necessarly need a record for aggregated records. we can pass documents along
+    public void addRecordsToCollection(ArrayList<Document> records, String collection) {
+
+        for(Document doc: records)
+        {
+            addRecordToCollection(doc, collection);
         }
     }
 
@@ -103,7 +123,8 @@ public class MongoClientMediator {
 
     // an int *should* suffice for now at least
     public int CollectionSize(String collection) {
-        // count is deprecated, there's an estimation which should work fine but it's not gonna be accurate
+        // count is deprecated, there's an estimation which should work fine but it's
+        // not gonna be accurate
         return (int) db.getCollection(collection).countDocuments();
     }
 
@@ -130,37 +151,34 @@ public class MongoClientMediator {
         for (int i = 0; i < colls.size(); i++) {
             System.out.println(colls.get(i));
         }
-        return mongoIteratorToStringArray(db.listCollectionNames());   
+        return mongoIteratorToStringArray(db.listCollectionNames());
     }
 
     // HELPER FUNCTIONS
     private String[] mongoIteratorToStringArray(MongoIterable iterable) {
         List<String> colls = new ArrayList<>();
 
-        
         iterable.forEach((Block<Document>) document -> colls.add(document.toJson().toString()));
-        
+
         return colls.toArray(new String[colls.size()]);
     }
 
-    //TODO: figure out a way to make this less wasteful
-    public  ArrayList<Record>  getCollectionAsRecordsArrayList(String collectionName)
-    {
+    // TODO: figure out a way to make this less wasteful
+    public ArrayList<Record> getCollectionAsRecordsArrayList(String collectionName) {
         Gson gson = new Gson();
 
         ArrayList<Record> records = new ArrayList<>();
 
         Type collType = getCollectionType(collectionName);
-        
-        String[] recordsToConvert = getCollection(collectionName);
-        
-        if(recordsToConvert.length == 0)
-            return null;
 
+        String[] recordsToConvert = getCollection(collectionName);
+
+        if (recordsToConvert.length == 0)
+            return null;
 
         for (int i = 0; i < recordsToConvert.length; i++) {
 
-            //p(recordsToConvert[i] + " of Type : " + collType.getTypeName());
+            // p(recordsToConvert[i] + " of Type : " + collType.getTypeName());
 
             records.add(gson.fromJson(recordsToConvert[i], collType));
         }
@@ -168,15 +186,16 @@ public class MongoClientMediator {
         return records;
     }
 
-    //Might be useful for the upper layers
-    public Type getCollectionType(String collectionName)
-    {
+    // Might be useful for the upper layers
+    public Type getCollectionType(String collectionName) {
         Type type = null;
-                
-        if(collectionName.contains("Alarm"))
-            type = new TypeToken<AlarmRecord>() {}.getType();
-        else 
-            type = new TypeToken<PacketRecord>() {}.getType();
+
+        if (collectionName.contains("Alarm"))
+            type = new TypeToken<AlarmRecord>() {
+            }.getType();
+        else
+            type = new TypeToken<PacketRecord>() {
+            }.getType();
 
         return type;
     }
