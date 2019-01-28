30f83ca6e2548744bc1d4b91b70f215e52e62eae
Mario <mario.ordiano@gmail.com>
2019-01-29 00:19:24 +0100

    changes to classes implementing aggregation

--- Back-end/src/main/java/invalid/adininspector/dataprocessing/NumberOfConnectionsPerNode.java
+++ Back-end/src/main/java/invalid/adininspector/dataprocessing/NumberOfConnectionsPerNode.java
@@ -2,13 +2,20 @@ package invalid.adininspector.dataprocessing;
 
 import java.util.ArrayList;
 
+import org.bson.Document;
+
 import invalid.adininspector.records.Record;
 
 public class NumberOfConnectionsPerNode implements IAggregator {
 
 	@Override
-	public ArrayList<Record> processData( ArrayList<Record>  records) {
-		return records;
+	public ArrayList<Document> processData(ArrayList<Record> records) {
+		return null;
+	}
+
+	@Override
+	public Document getNewAggregatorDocument(long tstmp) {
+		return null;
 	}
 
 }
---
+++ Back-end/src/main/java/invalid/adininspector/records/FlowratePerSecondRecord.java
@@ -0,0 +1,9 @@
+package invalid.adininspector.records;
+
+import java.util.Map;
+
+public class FlowratePerSecondRecord extends Record {
+    public int a;
+    private Map<String, String> connections;
+
+}
