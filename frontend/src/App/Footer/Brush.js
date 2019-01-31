7eaf12d030de0da3b441d11f3ff874e403740378
Xiaoru Li <dev@xiaoru.li>
2019-01-31 20:33:26 +0100

    Add notes

--- frontend/src/App/Footer/Brush.js
+++ frontend/src/App/Footer/Brush.js
@@ -2,6 +2,8 @@ import React, { PureComponent } from 'react';
 import styled from '@emotion/styled';
 import * as d3 from 'd3';
 
+// import { appStore, dataStore } from '@stores';
+
 const Container = styled.div`
   bottom: 0;
   display: flex;
@@ -15,10 +17,18 @@ export default class Brush extends PureComponent {
   brush = React.createRef();
 
   componentDidMount = () => {
+    // DO NOT TOUCH THE CODE ABOVE (except importing and change the bg color ofc)
+
     // use `this.brush.current` as the reference to the root node and that's it
+    // reference dataStore or appStore as needed, there shouldn't be any problems
+
     d3.select(this.brush.current)
       .append('span')
       .text('Hole punched, D3 plugged in. :)');
+
+    // do whatever you want :)
+
+    // DO NOT TOUCH THE CODE BELOW
   };
 
   render() {
