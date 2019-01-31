a99e5e052730721c2909a52cc1960fe7465b9e75
Xiaoru Li <dev@xiaoru.li>
2019-01-31 20:29:11 +0100

    Hole punched, D3 plugged in

--- frontend/src/App/Footer/Brush.js
+++ frontend/src/App/Footer/Brush.js
@@ -1,16 +1,31 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import styled from '@emotion/styled';
+import * as d3 from 'd3';
 
 const Container = styled.div`
   bottom: 0;
   display: flex;
   height: 5rem;
   width: 65rem;
+  /* TODO: you can remove the background color now */
   background-color: pink;
 `;
 
-export default class Brush extends Component {
+export default class Brush extends PureComponent {
+  brush = React.createRef();
+
+  componentDidMount = () => {
+    // use `this.brush.current` as the reference to the root node and that's it
+    d3.select(this.brush.current)
+      .append('span')
+      .text('Hole punched, D3 plugged in. :)');
+  };
+
   render() {
-    return <Container>I am brush.</Container>;
+    return (
+      <Container>
+        <div ref={this.brush} />
+      </Container>
+    );
   }
 }
