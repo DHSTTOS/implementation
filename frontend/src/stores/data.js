802a89b467c2645ca270ff1057231f327d00087b
Xiaoru Li <dev@xiaoru.li>
2019-02-05 00:06:09 +0100

    I've fucking got it to work

--- frontend/src/stores/data.js
+++ frontend/src/stores/data.js
@@ -11,17 +11,15 @@ class DataStore {
   availableCollections = []; //exampleCollection
 
   // Raw network data
-  @observable
-  //rawData = [];
+  @observable.shallow
   rawData = jsonstreams;
 
   @observable
   endpoints = []; // The start and end indices for the x-axis
 
-
-
   // The slice of raw data that is currently selected by the slider:
-  currentlySelectedData = ['1', '2'];
+  @observable.shallow
+  currentlySelectedData = jsonstreams;
 
   @observable
   sourceOptions = ['Source 1', 'Live'];
@@ -30,7 +28,7 @@ class DataStore {
   @action
   selectSource = source => {
     if (this.currentlySelectedSource === source) return;
-    
+
     appStore.resetDiagramConfigs();
     this.resetData();
     this.currentlySelectedSource = source;
