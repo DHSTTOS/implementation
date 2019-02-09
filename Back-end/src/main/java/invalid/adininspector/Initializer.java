44d231b97fea431d39c66affcefc8b4530564779
pm8008 <pm@currno.de>
2019-02-09 14:42:44 +0100

    Clean up the imports.

--- Back-end/src/main/java/invalid/adininspector/Initializer.java
+++ Back-end/src/main/java/invalid/adininspector/Initializer.java
@@ -4,9 +4,7 @@
  */
 package invalid.adininspector;
 
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.net.URL;
 import java.util.Properties;
 
 import invalid.adininspector.exceptions.LoginFailureException;
