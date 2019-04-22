84f76268cab0889002e1dd7c942633707022e969
pm8008 <pm@currno.de>
2019-04-23 01:48:53 +0200

    Update comment. Remove some commented-out code.

--- frontend/src/App/Footer/Brush.js
+++ frontend/src/App/Footer/Brush.js
@@ -90,6 +90,8 @@ export default class Brush extends PureComponent {
       // The dataset for the nodelink diagram is created on the
       // back-end; here we only send a request to the back-end
       // for the dataset for the current range:
+      // TODO: maybe we should do this only in brushended, not for
+      // every update while moving the brush (b/c of lag):
       console.log('getRIR:');
       getRecordsInRange(
         userStore.socket,
@@ -98,15 +100,6 @@ export default class Brush extends PureComponent {
         '' + tStart,
         '' + tEnd
       );
-
-      /*
-      tmp = dataStore.addressesAndLinksData.filter(
-        (x, i) => start <= i && i < end
-      );
-      */
-      // XXX currently no brushing/zooming for node-link diagram
-      // tmp = [...dataStore.addressesAndLinksData];
-      // dataStore.currentlySelectedAddressAndLinksData = tmp;
     };
 
     let updateCurrentRange = range => {
@@ -124,7 +117,6 @@ export default class Brush extends PureComponent {
     };
 
     let updateTotalRange = range => {
-      //console.log('updateCR: ' + range[0] + ', ' + range[1]);
       xTotalScale.domain(range);
       xAxisTotal.scale(xTotalScale).tickFormat(tickFormatTimeStampTotal);
       let t = d3.transition().duration(50); // XXX remove completely?
@@ -150,10 +142,6 @@ export default class Brush extends PureComponent {
     console.log('rawData length' + dataStore.rawData.length);
 
     let tickFormatTimeStamp = d => {
-      //console.log('tickFormatTimeStamp called, d:' + d + ' cr0 ' + curRange[0
-      //console.log("tickF: " + d + ": " + cSRD[d]);
-
-      //console.log('autorun.tickFormatTimeStamp:');
       if (appStore.brushConfig.tickstyle) {
         return '' + d;
       }
@@ -164,11 +152,6 @@ export default class Brush extends PureComponent {
       if (cSRD == null) return '';
 
       let offset = Math.floor(d - curRange[0]);
-      //console.log('cSRD.length: ' + cSRD.length + ' ' + curRange[0]);
-      //console.log(curRange);
-      //return d + '_' + (d-curRange[0]);
-      //console.log('offset: ' + offset);
-      // XXX workaround, if offset is too big:
       let tmpOffset = Math.min(offset, cSRD.length - 1);
       if (tmpOffset == -1) {
         return 'n/a';
@@ -185,7 +168,6 @@ export default class Brush extends PureComponent {
     };
 
     let tickFormatTimeStampTotal = d => {
-      //console.log('tFTST: ' + d + ' ' + dataStore.rawData.length);
       if (!dataStore.rawData.length) {
         return d;
       }
