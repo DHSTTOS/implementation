abf7d662e3d27b29d5df4807c82c80a7cc1970b0
pm8008 <pm@currno.de>
2019-04-23 11:18:56 +0200

    Revert "Make the nodelink react instanteously."
    This reverts commit 150641f26100f9f4c6029166163f09ec2d673f17.

--- frontend/src/components/Diagram/NodeLinkBlock.js
+++ frontend/src/components/Diagram/NodeLinkBlock.js
@@ -552,8 +552,7 @@ function Network() {
 
     node.attr('cx', d => d.x).attr('cy', d => d.y);
 
-    //if (e.alpha < 0.03) {
-    if (e.alpha < 0.93) {
+    if (e.alpha < 0.03) {
       updateLinks();
       force.stop();
     }
@@ -563,10 +562,7 @@ function Network() {
   // push them towards appropriate location.
   // Uses alpha to dampen effect over time.
   var moveToRadialLayout = function(alpha) {
-    // Force k=1.0 to jump directly to final position.
-    // PMXXX remove delay
-    //const k = alpha * 0.1;
-    const k = 1.0;
+    const k = alpha * 0.1;
     return function(d) {
       const centerNode = groupCenters(d.id);
       d.x += (centerNode.x - d.x) * k;
